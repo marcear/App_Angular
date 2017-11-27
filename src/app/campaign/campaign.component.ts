ff504d75928ad38380e3b67093e661ebeafa9642
Aluminé Castillo <acastillo@southplans.com.ar>
2017-11-27 11:02:10 -0300

    Hide and show main component

--- src/app/campaign/campaign.component.ts
+++ src/app/campaign/campaign.component.ts
@@ -7,21 +7,16 @@ import { Component, ViewChild } from '@angular/core';
 })
 
 export class CampaignComponent {
+
+  @ViewChild('navigation') navigationComponent;
   
-  arrowLeftIcon = "keyboard_arrow_left";
-  arrowRightIcon = "keyboard_arrow_right";
-  toogleIcon = this.arrowLeftIcon;
   title = "CAMPAIGN DETAILS";
   headerTitle="CAMPAIGNS";
   headerIcons: string[] = ['filter_list', 'add'];
   campaignIcons: string[] = ['dashboard', 'show_chart', 'delete', 'save', 'file_download', 'insert_chart', 'picture_as_pdf'];
-  
-  toggle(event) {
-    if (this.toogleIcon == this.arrowLeftIcon) {
-      this.toogleIcon = this.arrowRightIcon;
-    } else {
-      this.toogleIcon = this.arrowLeftIcon;
-    }
+ 
+  onChangeView() {
+    this.navigationComponent.toggle();
   }
 
 }
--- src/app/header-toolbar/header-toolbar.component.ts
+++ src/app/header-toolbar/header-toolbar.component.ts
@@ -9,8 +9,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class HeaderToolbarComponent {
 
   @Input() title: string;
-  @Input() icons: any[];
-  @Input() toogleIcon: string;
+  @Input() icons: any[];  
   @Input() displayToggle: boolean;
+  @Output() onToggle = new EventEmitter<void>();
+
+  arrowLeftIcon = "keyboard_arrow_left";
+  arrowRightIcon = "keyboard_arrow_right";
+  toogleIcon = this.arrowLeftIcon;
+
+  toggle():void {
+    if (this.toogleIcon == this.arrowLeftIcon) {
+      this.toogleIcon = this.arrowRightIcon;
+    } else {
+      this.toogleIcon = this.arrowLeftIcon;
+    }
+    this.onToggle.emit();
+  }
 
 }
