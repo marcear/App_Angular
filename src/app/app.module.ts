8ff71abf1cdd388d8497e399b2caf535f45a51d9
Laura Rodríguez <rdz.maria.laura@gmail.com>
2017-11-30 15:38:29 -0300

    Initial Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,52 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatMenuModule} from '@angular/material/menu';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { HttpModule} from '@angular/http';
+import { CalendarComponent} from "angular2-fullcalendar/src/calendar/calendar";
+import { MatDialogContent, MAT_PLACEHOLDER_GLOBAL_OPTIONS, MatInputModule} from '@angular/material';
+import { MatDialogModule} from '@angular/material/dialog';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import {EventService} from './eventService';
+import {CommonHelper} from "./commonHelper";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SchedulerComponent,
+    CalendarComponent,
+    DialogExampleComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    FormsModule
   ],
-  providers: [],
+   entryComponents: [
+     DialogExampleComponent
+  ],
+  providers: [
+		{provide: MAT_PLACEHOLDER_GLOBAL_OPTIONS, useValue: {float: 'always'}},
+    EventService, CommonHelper
+	],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/eventService.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {Event} from './event';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class EventService {
+
+    constructor() { }
+
+    getData(): Observable<Event[]> {
+        return of( [{ id: 1, resourceId: 'b', start: '2017-01-01T02:00:00', end: '2017-01-31T07:00:00', title: 'event 1', cost:0, description: 'una description'},
+                    { id: 2, resourceId: 'c', start: '2017-11-07T05:00:00', end: '2017-11-07T22:00:00', title: 'event 2' ,cost:0, description: ''}]);
+    }
+}
